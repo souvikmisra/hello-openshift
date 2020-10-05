fefc0a2564f63b4463367657a6ed7bcc2a5ff7c2
Avinash Reddy Palleti <avinash.reddy.palleti@intel.com>
2020-10-05 15:04:07 +0530

    update

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import os
 from flask import Flask
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
 def main():
 	os.system("deployment_tools/demo/demo_security_barrier_camera.sh -d CPU -sample-options -no_show")
 	return "Welcome, inference is running"
 
-@app.route('/how are you')
+@app.route('/dont run')
+def hello():
 	return 'Inference is not running'
 
 if __name__ == "__main__":
