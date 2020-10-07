c7b0105354793392477a5a9201a9c691db0c3870
Avinash Reddy Palleti <avinash.reddy.palleti@intel.com>
2020-10-07 12:45:01 +0530

    Fix check_output error

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-	p = subprocess.Popen("/bin/bash -c 'source /opt/intel/openvino/bin/setupvars.sh; cd /opt/pneumonia-classification/application/; python3 pneumonia_classification.py -m ../resources/FP32/model.xml -o /opt/output'", shell=True) 
+	p = subprocess.Popen("/bin/bash -c 'source /opt/intel/openvino/bin/setupvars.sh; cd /opt/pneumonia-classification/application/; python3 pneumonia_classification.py -m ../resources/FP16/model.xml -o /opt/output'", shell=True) 
 	p.wait()
-	output = subprocess.check_output("cat /opt/output/stats.txt")
-	return "Inference ran successfully "+ output
+	output = subprocess.check_output(['/bin/cat', '/opt/output/stats.txt'])
+	return "Inference completed. " + output.decode("utf-8")
 
 @app.route('/dont run')
 def hello():
