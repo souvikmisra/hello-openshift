ca5061b7545138c6ed8b2bacce9d2f321df0754b
Avinash Reddy Palleti <avinash.palleti@gmail.com>
2020-10-08 05:46:33 +0000

    Add result image response after inference

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,15 @@ USER root
 RUN apt-get update && apt-get install -y git python3-pip
 
 COPY app.py /opt/
+RUN mkdir -p /opt/templates
+COPY templates/index.html /opt/templates/
+
 WORKDIR /opt/
 RUN git clone https://github.com/intel-iot-devkit/brain-tumor-segmentations.git
 
 RUN pip3 install flask matplotlib tk numpy h5py
 
-RUN mkdir -p /opt/output && chmod 777 /opt/output
+RUN mkdir -p /opt/static && chmod 777 /opt/static
 
 WORKDIR /opt/intel/openvino/deployment_tools/model_optimizer/
 RUN python3 mo_tf.py --input_model /opt/brain-tumor-segmentations/resources/saved_model_frozen.pb  --output_dir /opt/brain-tumor-segmentations/resources/output/IR_models/FP32/ --input_shape=[1,144,144,4] --data_type FP32 --model_name saved_model
--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import os
 import subprocess
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-	p = subprocess.Popen("/bin/bash -c 'source /opt/intel/openvino/bin/setupvars.sh; cd /opt/brain-tumor-segmentations/application/; python3 brain_tumor_segmentation.py -m ../resources/output/IR_models/FP32/saved_model.xml -d CPU --data_file ../resources/Task01_BrainTumour.h5 -r /opt/output/'", shell=True) 
+	p = subprocess.Popen("/bin/bash -c 'source /opt/intel/openvino/bin/setupvars.sh; cd /opt/brain-tumor-segmentations/application/; python3 brain_tumor_segmentation.py -m ../resources/output/IR_models/FP32/saved_model.xml -d CPU --data_file ../resources/Task01_BrainTumour.h5 -r /opt/static/'", shell=True) 
 	p.wait()
-	output = subprocess.check_output(['/bin/cat', '/opt/output/stats.txt'])
+	output = subprocess.check_output(['/bin/cat', '/opt/static/stats.txt'])
 	return "Inference completed. " + output.decode("utf-8")
 
 @app.route('/dont run')
 def hello():
 	return 'Inference is not running'
+@app.route('/result')
+def show_index():
+    full_filename = 'pred0.png'
+    return render_template("index.html", user_image = full_filename)
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8080)
